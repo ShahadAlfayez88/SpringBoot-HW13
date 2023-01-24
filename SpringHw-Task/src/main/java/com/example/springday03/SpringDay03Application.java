31986b5bff1be36362cfc8cc92b0b0a65897ee4f
Shahad Alfayez <89224138+ShahadAlfayez88@users.noreply.github.com>
2023-01-24 21:44:36 +0300

    Add files via upload

---
+++ SpringHw-Task/src/main/java/com/example/springday03/SpringDay03Application.java
@@ -0,0 +1,13 @@
+package com.example.springday03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDay03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDay03Application.class, args);
+    }
+
+}
